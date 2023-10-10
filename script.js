2d9481e19b691449258764acc7f6e82283a372c0
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-10-10 18:55:44 -0400

    Added num-btn events

--- script.js
+++ script.js
@@ -16,4 +16,23 @@ function operate(a, operation, b){
     return operation(a, b);
 }
 
-console.log(operate(5, add, 3));
+let firstNum =""; //Make firstNum be equal to the firstNum they enter and the ensuing results
+//Its a string cause when we click a button, it would return string. We also want "1" + "1" = "11"
+let operator="";
+let secondNum="";
+
+
+const numBtns = document.querySelectorAll(".number");
+numBtns.forEach(btn =>{
+    btn.addEventListener("click", () => {
+        firstNum = firstNum + btn.textContent;
+        updateDisplay();
+        })
+})
+//Note that btn.textContent is a string which is ok because we want "1" + "1" = "11"
+//Each click, we update our firstNum 
+
+function updateDisplay(){
+    document.querySelector("#current-input").textContent=firstNum;
+}
+
